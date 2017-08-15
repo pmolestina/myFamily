5646ce5ebe48ca020e99aeda2cbcde009bd07f88
pmolestina <pepe_molestina@hotmail.com>
2017-08-15 17:09:18 -0400

    a

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,45 +1,63 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, LoadingController, ToastController } from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
- 
+
 @Component({
   selector: 'page-main',
   templateUrl: 'register.html'
 })
 export class RegisterPage {
   createSuccess = false;
-  registerCredentials = {email: '', password: ''};
- 
-  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController) {}
- 
+  registerCredentials = { email: '', password: '', displayName: '' };
+
+  constructor(private nav: NavController, private auth: AuthService,
+    private alertCtrl: AlertController, public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController) {
+
+  }
+
   public register() {
+    var toaster = this.toastCtrl.create({
+      duration:3000,
+      position: 'bottom'
+    })
+    if(this.registerCredentials.password.length <6){
+      toaster.setMessage('Password needs at least 6 characters');
+      toaster.present();
+      return;
+    }
+    let loader = this.loadingCtrl.create({
+      content: 'Please wait'
+    })
+    loader.present();
     this.auth.register(this.registerCredentials).subscribe(response => {
+      loader.dismiss();
       if (response.validRequest) {
         this.createSuccess = true;
-          this.showPopup("Success", "Account created. You can login now with your credentials.");
+        this.showPopup("Success", "Account created. You can login now with your credentials.");
       } else {
         this.showPopup("Error", "Problem creating account. " + response.message);
       }
     },
-    error => {
-      this.showPopup("Error", error);
-    });
+      error => {
+        this.showPopup("Error", error);
+      });
   }
- 
+
   showPopup(title, text) {
     let alert = this.alertCtrl.create({
       title: title,
       subTitle: text,
       buttons: [
-       {
-         text: 'OK',
-         handler: data => {
-           if (this.createSuccess) {
-             this.nav.popToRoot();
-           }
-         }
-       }
-     ]
+        {
+          text: 'OK',
+          handler: data => {
+            if (this.createSuccess) {
+              this.nav.popToRoot();
+            }
+          }
+        }
+      ]
     });
     alert.present();
   }
--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFire, AuthProviders, AuthMethods, FirebaseApp } from 'angularfire2';
 import 'rxjs/add/operator/map';
+import firebase from 'firebase';
 
 export class ValidationResponse {
   validRequest: boolean;
@@ -17,9 +18,11 @@ export class ValidationResponse {
 export class AuthService {
   firebaseAuth: any;
   userProfile: any;
-  authConfig:any;
+  authConfig: any;
+  firedata = firebase.database().ref('/appusers');
+
   get currentUser(): any {
-   
+
     return this.firebaseAuth.currentUser;
   }
   constructor(public af: AngularFire, @Inject(FirebaseApp) fa: any) {
@@ -31,7 +34,7 @@ export class AuthService {
 
   }
   public login(credentials) {
-    
+
     let validationResponse = new ValidationResponse("login");
     if (credentials.email === null || credentials.password === null) {
       return Observable.throw("Please insert credentials");
@@ -53,42 +56,51 @@ export class AuthService {
     }
   }
   //this is another way of login in using promise instead of observable
-  public loginWithPromise(credentials){
-    return new Promise((resolve, reject)=>{
-      this.af.auth.login(credentials, this.authConfig).then(()=>{
+  public loginWithPromise(credentials) {
+    return new Promise((resolve, reject) => {
+      this.af.auth.login(credentials, this.authConfig).then(() => {
         resolve(true);
-      }).catch((err)=>{
+      }).catch((err) => {
         reject(err);
       })
     });
   }
 
   public updateUserProfile(profile) {
-    var self=this;
-    return this.firebaseAuth.currentUser.updateProfile(profile);
-    
+    var self = this;
+    return this.firebaseAuth.currentUser.updateProfile(profile).then(() => {
+      self.firedata.child(self.firebaseAuth.currentUser.uid).update(profile)
+    });
   }
 
   public register(credentials) {
     let validationResponse = new ValidationResponse("register");
-    if (credentials.email === null || credentials.password === null) {
-      return Observable.throw("Please insert credentials");
-    } else {
-      return Observable.create(observer => {
-        let self = this;
-        this.af.auth.createUser(credentials).then(function (response) {
-          console.log(response);
-          validationResponse.validRequest = true;
-          observer.next(validationResponse);
-          observer.complete();
-        }).catch(function (error) {
-          console.log(error);
-          validationResponse.message = error.message;
-          observer.next(validationResponse);
-          observer.complete();
-        });
+    var defaultPhotoURL='https://firebasestorage.googleapis.com/v0/b/thefamily-eb9bb.appspot.com/o/images%2Fuser.png?alt=media&token=b3236d20-c28b-4022-8b81-4ff013896b64';
+    return Observable.create(observer => {
+      let self = this;
+      this.af.auth.createUser(credentials).then(function (response) {
+        self.firebaseAuth.currentUser.updateProfile({
+          displayName: credentials.displayName,
+          photoURL: defaultPhotoURL
+        }).then(() => {
+          self.firedata.child(self.firebaseAuth.currentUser.uid).set({
+            uid: self.firebaseAuth.currentUser.uid,
+            displayName: credentials.displayName,
+            photoURL: defaultPhotoURL
+          })
+        })
+        console.log(response);
+        validationResponse.validRequest = true;
+        observer.next(validationResponse);
+        observer.complete();
+      }).catch(function (error) {
+        console.log(error);
+        validationResponse.message = error.message;
+        observer.next(validationResponse);
+        observer.complete();
       });
-    }
+    });
+
   }
   public reset(credentials) {
     let validationResponse = new ValidationResponse("reset");
