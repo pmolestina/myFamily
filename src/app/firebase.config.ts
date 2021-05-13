951d263df33fc284c0ae63cc6526b62b77bf4e62
pmolestina <pepe_molestina@hotmail.com>
2021-05-13 10:07:17 -0400

    Delete firebase.config.ts

--- src/app/firebase.config.ts
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig={
-    apiKey: "AIzaSyDgFG2F9dAG2qcD-gFuRgFrGK8xWdN7OTY",
-    authDomain: "thefamily-eb9bb.firebaseapp.com",
-    databaseURL: "https://thefamily-eb9bb.firebaseio.com",
-    storageBucket: "thefamily-eb9bb.appspot.com",
-    messagingSenderId: "91662480324"
-};
